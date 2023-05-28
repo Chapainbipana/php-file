13d77e2037e6015741a9e4a82ee2cdebffcdc1f9
bipana chapain <111263851+Chapainbipana@users.noreply.github.com>
2023-05-28 18:51:00 +0545

    Update admin.php

--- admin.php
+++ admin.php
@@ -8,26 +8,16 @@
   	$password = trim( $_POST['password']);
     $_SESSION['name']=$name;
     $_SESSION['password']=$password;
-    $q = " SELECT * FROM `admin`";
-      
+    $q = " SELECT * FROM `admin`"; 
       $query= mysqli_query($connection,$q);
-     
-      
       while( $res=mysqli_fetch_array($query))
       {
-           
-  
-
           if($res['name']==$name && $res['password']==$password )
           {
-        
-      
-            header("location:studentform.php");
+            header("location:record.php");
           }
-          
       }
     }else{
       echo "enter the username and password ";
     }
-
-?>	     
+?>	   
